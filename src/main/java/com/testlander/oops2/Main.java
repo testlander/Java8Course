756e51166d2f37e7c2a44557e34407b19d2ccd1c
meteb <mete.boyaci@testlander.co.uk>
2019-12-18 23:36:52 +0000

    oops sample

---
+++ src/main/java/com/testlander/oops2/Main.java
@@ -0,0 +1,23 @@
+package com.testlander.oops2;
+
+public class Main {
+    public static void main(String[] args) {
+//        Hamburger hamburger = new Hamburger("Basic", "Steak", 3.56, "White");
+//        double price = hamburger.itimizeHamburger();
+//        hamburger.addHamburgerAddition1("Tomato", 0.27);
+//        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+//        hamburger.addHamburgerAddition3("Cheese", 1.13);
+//        System.out.println("Total burger price is " + hamburger.itimizeHamburger());
+
+//        HealthyBurger healthyBurger = new HealthyBurger("Chicken", 5.67);
+//        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+//        healthyBurger.addHealthyAddition1("Latituce", 4.0);
+//        System.out.println("Total healty hamburger price: " + healthyBurger.itimizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.itimizeHamburger();
+
+
+    }
+
+}
